5a4eb620d013e9f123516b0b44e5e9e077b4a3e6
motah-fard <motah.fard@gmail.com>
2022-06-07 14:21:47 -0700

    func

--- smallProjects/cards/main.go
+++ smallProjects/cards/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	card := "ace of spades"
-	card = "five of diamonds"
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "yooohooo"
 }
