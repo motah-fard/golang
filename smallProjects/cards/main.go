3ef936d4b4989cc2cfd2e1c855b9f78b80386719
motah-fard <motah.fard@gmail.com>
2022-06-09 10:53:23 -0700

    OOP

--- smallProjects/cards/main.go
+++ smallProjects/cards/main.go
@@ -1,15 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
-	card := newCard()
-	cards := []string{newCard(), "ace of diamonds"}
+	// 	card := newCard()
+	// []string is equal to deck
+	cards := deck{newCard(), "ace of diamonds"}
 	cards = append(cards, "six six six")
-	for i, myCard := range cards {
-		fmt.Println(i, myCard)
-	}
-	fmt.Println(cards, card)
+	cards.print()
+	// fmt.Println(cards, card)
 }
 
 func newCard() string {
