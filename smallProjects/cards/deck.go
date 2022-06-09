28fe1ce93e5e59806fb7cbd6e630de6d36238db6
motah-fard <motah.fard@gmail.com>
2022-06-09 12:26:11 -0700

    deck

--- smallProjects/cards/deck.go
+++ smallProjects/cards/deck.go
@@ -6,6 +6,23 @@ import "fmt"
 //  which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	// as slice of strings
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	// as slice of strings
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Sarbaz", "Queen", "King"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+	return cards
+}
+
+// reciever on a function
+// d is like this or self in python or JS/ but we call it the actual thing that it is
+// d is the abbreviation of deck cause cards are part of deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- smallProjects/cards/main.go
+++ smallProjects/cards/main.go
@@ -3,12 +3,14 @@ package main
 func main() {
 	// 	card := newCard()
 	// []string is equal to deck
-	cards := deck{newCard(), "ace of diamonds"}
-	cards = append(cards, "six six six")
-	cards.print()
+	// cards := deck{newCard(), "ace of diamonds"}
+	// cards = append(cards, "six six six")
+	// cards.print()
 	// fmt.Println(cards, card)
+	cards := newDeck()
+	cards.print()
 }
 
-func newCard() string {
-	return "yooohooo"
-}
+// func newCard() string {
+// 	return "yooohooo"
+// }
