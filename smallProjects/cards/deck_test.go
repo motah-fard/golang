d7e307152c3b4b8c6e373c5cb9ded4dbfdd580e0
motah-fard <motah.fard@gmail.com>
2022-06-13 13:07:12 -0700

    _

--- smallProjects/cards/deck_test.go
+++ smallProjects/cards/deck_test.go
@@ -12,6 +12,13 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != x {
 		t.Errorf("Expected deck length of %x, but got %d", x, len(d))
 	}
+	c := "Spades of Ace"
+	if d[0] != c {
+		t.Errorf("Expected first card of Spades of Ace but got %v", d[0])
+	}
+	if d[len(d)-1] != "Clubs of King" {
+		t.Errorf("Expected last card of Four of Spades but got %v", d[len(d)-1])
+	}
 }
 
 // first card is Ace of spades
