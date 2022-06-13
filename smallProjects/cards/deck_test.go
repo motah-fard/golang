0943f13012c9a9ff14ea214d89a3ab8fe30a208b
motah-fard <motah.fard@gmail.com>
2022-06-13 14:51:14 -0700

    :D

--- smallProjects/cards/deck_test.go
+++ smallProjects/cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // make sure the deck is created with X amount of cards
 // every function starts with capital T
@@ -12,15 +15,40 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != x {
 		t.Errorf("Expected deck length of %x, but got %d", x, len(d))
 	}
+	// first card is Ace of spades
 	c := "Spades of Ace"
 	if d[0] != c {
 		t.Errorf("Expected first card of Spades of Ace but got %v", d[0])
 	}
+	// last card is four of clubs
 	if d[len(d)-1] != "Clubs of King" {
 		t.Errorf("Expected last card of Four of Spades but got %v", d[len(d)-1])
 	}
 }
 
-// first card is Ace of spades
-// last card is four of clubs
 // test save to deck and new deck from file
+//  create deck
+//  save to file
+//  file created
+// attempt to load load file
+// crash
+
+// delete any files with the name decktesting
+// create deck
+// save to file
+// load from file
+// assert deck length
+// delete any files in current working directory with name
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	x := 52
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != x {
+		t.Errorf("Expected deck length of %x, but got %v", x, len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
